53b63308aed1b91a3ef29732e81b497478e9f5a5
hoangvanthohvt-star <hoangvantho.hvt@gmail.com>
2026-05-17 22:11:53 +0700

    Add VNIndex vs Breadth chart (% above MA50, 1Y/2Y/3Y toggle)

--- scripts/render.py
+++ scripts/render.py
@@ -535,6 +535,7 @@ def main():
         # Charts
         "{{chart_json}}":            chart_json,
         "{{nhnl_chart_json}}":       json.dumps(snapshot.get("nhnl_chart", {})),
+        "{{breadth_chart_json}}":    json.dumps(snapshot.get("breadth_chart", {})),
         # Divergence checklist
         "{{regime_div5d_rows}}":         render_div_direction_rows(d5),
         "{{regime_div5d_verdict_class}}": v5_cls,
